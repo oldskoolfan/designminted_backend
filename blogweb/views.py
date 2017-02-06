4676b7748fce7c552b1297d452c5701d75483af5
Andrew Harris <harris.1305@gmail.com>
2017-02-05 21:22:45 -0500

    owl carousel plugin, images upload to file system instead of db

--- blogweb/views.py
+++ blogweb/views.py
@@ -1,15 +1,19 @@
 from django.shortcuts import render
 from django.views.generic import TemplateView, View
 from django.core.mail import EmailMessage
-from django.http import HttpResponse, HttpResponseRedirect, HttpResponseServerError
+from django.http import HttpResponse, HttpResponseRedirect, JsonResponse
 from blogapi.models import *
 from forms import ContactForm
 from django.views.decorators.gzip import gzip_page
 from django.utils.decorators import method_decorator
 from django.conf import settings
+from PIL import Image
 import PyRSS2Gen
 import MyRSS2
 import datetime
+import re
+import os
+from django.conf import settings as djangoSettings
 
 # Create your views here.
 
@@ -20,9 +24,12 @@ class PublicBaseView(TemplateView):
         context = { "blog": blog }
         return render(request, self.template_name, context)
 
-class HomeView(PublicBaseView):
-    page_type = Blog.HOME
+class HomeView(TemplateView):
     template_name = "home.html"
+    def get(self, request, *args, **kwargs):
+        items = Blog.objects.filter(page_type=Blog.PORTFOLIO).first().contents.all()[:3]
+        context = { 'items': items }
+        return render(request, self.template_name, context)
 
 class ServicesView(PublicBaseView):
     page_type = Blog.SERVICES
@@ -35,11 +42,13 @@ class AboutView(PublicBaseView):
 class BlogPageView(TemplateView):
     template_name = "blog.html"
     def get(self, request, *args, **kwargs):
-        if 'id' in kwargs:
-            blogs = [Blog.objects.get(pk=kwargs['id'])]
-        else:
-            blogs = Blog.objects.filter(page_type=Blog.BLOG).order_by('-pub_date')
-        context = { "blogs": blogs }
+        # todo: add blogger feed to this page
+        # if 'id' in kwargs:
+        #     blogs = [Blog.objects.get(pk=kwargs['id'])]
+        # else:
+        #     blogs = Blog.objects.filter(page_type=Blog.BLOG).order_by('-pub_date')
+        # context = { "blogs": blogs }
+        context = { 'blogs': [] }
         return render(request, self.template_name, context)
 
 class PortfolioView(PublicBaseView):
@@ -119,3 +128,25 @@ class RssFeedView(View):
         rss.rss_attrs['xmlns:atom'] = "http://www.w3.org/2005/Atom"
 
         return HttpResponse(rss.to_xml(), content_type="application/rss+xml")
+
+class UploadImageView(View):
+    def post(self, request):
+        path = self.resizeImage(request.FILES['image_upload'])
+        return JsonResponse({ 'location': path })
+
+    def resizeImage(self, upload):
+        limit = 500000
+        img = Image.open(upload.file)
+        imgPath = "{0}{1}".format(djangoSettings.MEDIA_ROOT, upload.name)
+        img.save(imgPath)
+        width, height = img.size
+        ratio = float(width) / float(height)
+        quality = 100
+        while os.path.getsize(imgPath) > limit:
+            width -= 100
+            quality -= 10
+            height = int(width / ratio)
+            img = img.resize((width, height), Image.ANTIALIAS)
+            img.save(imgPath, quality=quality)
+            img = Image.open(imgPath)
+        return re.sub('blogweb', '', imgPath, 1)
